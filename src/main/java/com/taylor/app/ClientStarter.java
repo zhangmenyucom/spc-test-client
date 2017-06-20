ae7a406149057a490ed0d03b0808458829a75854
xiaolu.zhang <516195940@qq.com>
2017-06-20 20:16:42 +0800

    更新

--- src/main/java/com/taylor/app/ClientStarter.java
+++ src/main/java/com/taylor/app/ClientStarter.java
@@ -3,6 +3,7 @@ package com.taylor.app;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,13 +11,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @RestController
 @EnableSwagger2
+@EnableEurekaClient
 public class ClientStarter {
+
+    @Value("${content}")
+    private String content;
+
     @Value("${name}")
-    String bar;
+    private String bar;
 
     @RequestMapping("/")
     String hello() {
-        return "Hello " + bar + "!" ;
+
+        return "Hello " + bar + "!"+content;
     }
 
     public static void main(String... args) {
--- src/main/java/com/taylor/app/Swagger.java
+++ src/main/java/com/taylor/app/Swagger.java
@@ -1,6 +1,7 @@
 package com.taylor.app;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@ComponentScan(basePackages = "com.taylor")
 public class Swagger {
 
     @Bean
