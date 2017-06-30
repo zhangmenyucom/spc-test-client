83bba1b193cd30723d36e8c8e6410fc68ff1dac7
xiaolu.zhang <516195940@qq.com>
2017-06-30 19:13:46 +0800

    更新

--- src/main/java/com/taylor/app/ClientStarter.java
+++ src/main/java/com/taylor/app/ClientStarter.java
@@ -3,6 +3,7 @@ package com.taylor.app;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @RestController
 @EnableSwagger2
 @EnableEurekaClient
+@RefreshScope
 public class ClientStarter {
 
     @Value("${content}")
